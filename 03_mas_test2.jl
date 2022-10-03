e0649f870a7402c9a78dba7d27cdc40387ee5777
ParachutePenguin <sst7092000@yahoo.co.jp>
2022-10-03 12:14:27 +0000

    test

--- 03_mas_test2.jl
+++ 03_mas_test2.jl
@@ -1,3 +1,5 @@
+using Pkg
+
 Pkg.add("Agents")
 Pkg.add("Distributions")
 
@@ -20,3 +22,20 @@ end
 
 lamb = 0.4
 d_lamb = Bernoulli(lamb)
+
+function initialize_model(;
+    seed = 0,
+    lamb = 0.7,
+    lamb_fil = 0.5,
+    th = 0.95,
+    th_fil = 0.5,
+    failure = 0,
+    theta = 0.95
+    )
+end
+
+function agent_step!(agent,model)
+
+end
+
+model = ABM(Union{OSS,Dev})
