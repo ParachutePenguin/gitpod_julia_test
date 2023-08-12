d7ca8ee737abaa3004babff8456bacd2214662df
ParachutePenguin <sst7092000@yahoo.co.jp>
2023-08-12 10:35:33 +0000

    51_ga_test.jl

---
+++ 04_mas_test3.jl
@@ -0,0 +1,65 @@
+import Pkg;
+Pkg.add("Distributions")
+Pkg.add("Plots")
+using Distributions, Plots
+
+# OSSエージェント用の構造体を定義します。フィールドは故障率と累積修復故障数です。
+mutable struct NewMutableOSSAgent
+    failure_rate::Float64
+    resolved_failures::Int64
+end
+
+# 開発者エージェント用の構造体を定義します。フィールドは修復した故障数です。
+mutable struct NewDeveloperAgent
+    resolved_failures::Int64
+end
+
+# シミュレーションの各ステップを進行させる関数です。
+function step!(oss::NewMutableOSSAgent, dev::NewDeveloperAgent)
+    # OSSエージェントがポアソン分布に従って故障を生成します。
+    failures = rand(Poisson(oss.failure_rate))
+
+    # 開発者エージェントが次のタイムステップで故障を修復します。
+    fix_failures!(oss, dev, failures)
+
+    return failures
+end
+
+# 開発者エージェントが故障を修復する関数です。
+function fix_failures!(oss::NewMutableOSSAgent, dev::NewDeveloperAgent, failures::Int64)
+    # OSSエージェントと開発者エージェントの修復数を更新します。
+    oss.resolved_failures += failures
+    dev.resolved_failures = failures
+end
+
+# シミュレーションを実行する関数です。
+function run_simulation(oss::NewMutableOSSAgent, dev::NewDeveloperAgent, steps::Int64)
+    history_failures = []
+    history_resolved = []
+
+    for t in 1:steps
+        failures = step!(oss, dev)
+        push!(history_failures, failures)
+        push!(history_resolved, dev.resolved_failures)
+    end
+
+    return history_failures, history_resolved
+end
+
+# 故障率0.1のOSSエージェントと開発者エージェントを作成します。
+oss = NewMutableOSSAgent(0.1, 0)
+dev = NewDeveloperAgent(0)
+
+# 1000ステップのシミュレーションを実行します。
+history_failures, history_resolved_per_step = run_simulation(oss, dev, 1000)
+
+# 修復された故障数の累積履歴を作成します。
+history_resolved_cumulative = cumsum(history_resolved_per_step)
+
+# 二つのy軸を持つグラフを作成します。
+p = plot(history_failures, label="Failures per step", title="OSS Development Simulation", xlabel="Time Step", ylabel="Failures & Resolved per step", legend=:top)
+plot!(history_resolved_per_step, label="Resolved per step")
+p2 = twinx()
+plot!(p2, history_resolved_cumulative, label="Cumulative resolved", color=:green, ylabel="Cumulative resolved", legend=:topright)
+
+display(p)
---
+++ 51_ga_test.jl
@@ -0,0 +1,38 @@
+# 一様分布に基づく乱数の集団を生成する関数
+function rand_population_uniform(m, a, b)
+    # 配列aの次元数を取得
+    d = length(a)
+    # 各次元ごとに一様乱数を生成して、集団として返す
+    return [a + rand(d).*(b-a) for i in 1:m]
+end
+
+# 必要なライブラリをインポート
+import Pkg
+Pkg.add("Distributions")
+using Distributions
+
+# 多変量正規分布に基づく乱数の集団を生成する関数
+function rand_population_normal(m, µ, ∑)
+    # 多変量正規分布の定義
+    D = MvNormal(µ, ∑)
+    # 多変量正規分布に基づく乱数を生成して、集団として返す
+    return [rand(D) for i in 1:m]
+end
+
+# コーシー分布に基づく乱数の集団を生成する関数
+function rand_population_cauchy(m, µ, σ)
+    # 配列µの次元数を取得
+    n = length(µ)
+    # 各次元ごとにコーシー乱数を生成して、集団として返す
+    return [[rand(Cauchy(μ[j], σ[j])) for j in 1:n] for i in 1:m]
+end
+
+import Pkg
+Pkg.add("Plots")
+using Plots
+
+# 一様分布に基づく乱数を生成
+population_uniform = rand_population_uniform(1000, [0, 0], [1, 1])
+x = [pt[1] for pt in population_uniform]
+y = [pt[2] for pt in population_uniform]
+scatter(x, y, title="Uniform Distribution")
